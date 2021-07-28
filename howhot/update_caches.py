33595a4abfd257573a6137c3732bd5de8d486166
Matt Bachmann <bachmann.matt@gmail.com>
2021-07-27 20:01:30 -0400

    Add emailing on cache update failures. Introducing intential failure for test

--- howhot/update_caches.py
+++ howhot/update_caches.py
@@ -1,14 +1,16 @@
 import os
+import traceback
 
+from redis import Redis
 from redis import from_url as get_redis_from_url
+from sendgrid import Mail, SendGridAPIClient
 
 from howhot.device_stats import update_battery_cache
 from howhot.shop_temp import update_shop_cache
 from howhot.weather import update_weather_cache
 
 
-def main():
-    redis = get_redis_from_url(os.environ["REDIS_URL"])
+def do_update(redis: Redis) -> None:
     weather = update_weather_cache(
         redis,
         os.environ["WEATHER_LAT"],
@@ -32,6 +34,24 @@ def main():
     )
     print("Updated Battery Cache!")
     print(f"Battery Level {battery}")
+
+
+def main() -> None:
+    redis = get_redis_from_url(os.environ["REDIS_URL"])
+    try:
+        dog = [0]
+        print(dog[221])  # Intentional error for live test
+        do_update(redis)
+    except Exception:
+        trace = traceback.format_exc()
+        message = Mail(
+            from_email=os.environ["ADMIN_EMAIL"],
+            to_emails=[os.environ["SENDER"]],
+            subject="Hot Hot Is The Shop Error",
+            plain_text_content=trace,
+        )
+        SendGridAPIClient(os.environ["SENDGRID_API_KEY"]).send(message)
+        raise
 
 
 if __name__ == "__main__":
